af96b72e171852aab6330e94243c6e0a235374dd
mohamedhakm192-droid <mohamedhakm192@gmail.com>
2026-04-22 19:59:53 +0200

    Update •	core.py

--- docs/paper/system/•	core.py
+++ docs/paper/system/•	core.py
@@ -1,1 +1,18 @@
 
+import numpy as np
+
+# Laplacian Matrix
+L = np.array([
+    [2, -1, -1],
+    [-1, 2, -1],
+    [-1, -1, 2]
+])
+
+# Energy Function
+def energy(X):
+    return X.T @ L @ X
+
+# PAC Transformation
+def pac(X, eta=0.2):
+    mean = np.mean(X)
+    return X + eta * (mean - X)
